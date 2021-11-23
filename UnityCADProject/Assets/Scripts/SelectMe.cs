b3c1e4dede97c5f04c166b33c524e4a84394ebc6
KiShiVi <k.shirnin@gmail.com>
2021-11-23 07:37:30 +0300

    method extraction

--- UnityCADProject/Assets/Scripts/SelectMe.cs
+++ UnityCADProject/Assets/Scripts/SelectMe.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine.UI;
 using UnityEngine;
 
 public class SelectMe : MonoBehaviour
@@ -8,16 +7,6 @@ public class SelectMe : MonoBehaviour
     public GameObject mainScript;
     public Material selectionMaterial;
     public Material standartMaterial;
-
-    public Text positionX;
-    public Text positionY;
-    public Text positionZ;
-    public Text rotationX;
-    public Text rotationY;
-    public Text rotationZ;
-    public Text scaleX;
-    public Text scaleY;
-    public Text scaleZ;
 
     public void OnMouseUp()
     {
@@ -31,16 +20,6 @@ public class SelectMe : MonoBehaviour
         }
         mainScript.GetComponent<StoringTheSelectedShape>().selectionShape = this.gameObject;
 
-        positionX.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().position.x).ToString();
-        positionY.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().position.y).ToString();
-        positionZ.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().position.z).ToString();
-
-        rotationX.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().rotation.eulerAngles.x).ToString();
-        rotationY.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().rotation.eulerAngles.y).ToString();
-        rotationZ.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().rotation.eulerAngles.z).ToString();
-
-        scaleX.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().localScale.x).ToString();
-        scaleY.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().localScale.y).ToString();
-        scaleZ.text = Mathf.RoundToInt(this.gameObject.GetComponent<Transform>().localScale.z).ToString();
+        mainScript.GetComponent<StoringTheSelectedShape>().fillContent(this.gameObject);
     }
 }
--- UnityCADProject/Assets/Scripts/StoringTheSelectedShape.cs
+++ UnityCADProject/Assets/Scripts/StoringTheSelectedShape.cs
@@ -1,11 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoringTheSelectedShape : MonoBehaviour
 {
     public GameObject selectionShape;
     public GameObject properties;
+
+    public Text positionX;
+    public Text positionY;
+    public Text positionZ;
+    public Text rotationX;
+    public Text rotationY;
+    public Text rotationZ;
+    public Text scaleX;
+    public Text scaleY;
+    public Text scaleZ;
 
     void Start()
     {
@@ -17,4 +28,20 @@ public class StoringTheSelectedShape : MonoBehaviour
     {
         properties.SetActive(selectionShape == null ? false : true);
     }
+
+    public void fillContent(GameObject obj)
+    {
+        positionX.text = Mathf.RoundToInt(obj.GetComponent<Transform>().position.x).ToString();
+        positionY.text = Mathf.RoundToInt(obj.GetComponent<Transform>().position.y).ToString();
+        positionZ.text = Mathf.RoundToInt(obj.GetComponent<Transform>().position.z).ToString();
+
+        rotationX.text = Mathf.RoundToInt(obj.GetComponent<Transform>().rotation.eulerAngles.x).ToString();
+        rotationY.text = Mathf.RoundToInt(obj.GetComponent<Transform>().rotation.eulerAngles.y).ToString();
+        rotationZ.text = Mathf.RoundToInt(obj.GetComponent<Transform>().rotation.eulerAngles.z).ToString();
+
+        scaleX.text = Mathf.RoundToInt(obj.GetComponent<Transform>().localScale.x).ToString();
+        scaleY.text = Mathf.RoundToInt(obj.GetComponent<Transform>().localScale.y).ToString();
+        scaleZ.text = Mathf.RoundToInt(obj.GetComponent<Transform>().localScale.z).ToString();
+    }
+
 }
